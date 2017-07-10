300339078e0f40d6a8f9b438063f01ab1436be96
hea <1544500164@qq.com>
2017-07-10 10:26:00 +0800

    add font

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,12 @@
 <template>
 	<header class="clearfix">
-		<span class="fll">&lt;</span>
+		<span class="fll back" @click="returnBack">
+      <i class="icon iconfont icon-back"></i>
+    </span>
     <div class="searchWrap" v-show="showSearch">
       <input type="text" v-model="searchText" @keyup.stop.prevent.13="emitSearch">
     </div>
+    <h1 class="title" v-show="!showSearch">111</h1>
 	</header>
 </template>
 <script type="text/javascript">
@@ -35,5 +38,34 @@ export default {
 }
 </script>
 <style type="text/css" scoped>
+header{
+  height: 42px;
+  padding: 0 6px;
+  box-sizing: border-box;
+}
+.back{
+  height: 100%;
+  padding: 13px 6px;
+  box-sizing: border-box;
+}
+.searchWrap{
+  width: 75%;
+  height: 100%;
+  margin: auto;
+  padding-top: 10px;
+  box-sizing: border-box;
+}
+.searchWrap input{
+  border:none;
+  outline: none;
+  border-bottom: 1px solid #e2e2e2;
+  width: 100%;
+  height: 24px;
+  line-height: 24px;
+  font-size: 14px;
+}
+
+
+
 
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 import './assets/styles/reset.css'
 import 'animate.css'
+import './assets/styles/fonts/iconfont.css'
 
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
